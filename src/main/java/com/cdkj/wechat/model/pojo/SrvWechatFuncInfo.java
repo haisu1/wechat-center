bfe8727dd36fdce862a279d39658f502f99cb1cc
haisu1 <603282533@qq.com>
2018-05-28 20:24:46 +0800

    Create SrvWechatFuncInfo.java

---
+++ src/main/java/com/cdkj/wechat/model/pojo/SrvWechatFuncInfo.java
@@ -0,0 +1,123 @@
+package com.cdkj.wechat.model.pojo;
+
+public class SrvWechatFuncInfo {
+    private String id;
+
+    private String sysAccount;
+
+    private String mainId;
+
+    private Integer funcId;
+
+    private Integer enabled;
+
+    private Integer version;
+
+    private Integer status;
+
+    private String remark;
+
+    private String memo;
+
+    private String createBy;
+
+    private String createDt;
+
+    private String updateDt;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getSysAccount() {
+        return sysAccount;
+    }
+
+    public void setSysAccount(String sysAccount) {
+        this.sysAccount = sysAccount == null ? null : sysAccount.trim();
+    }
+
+    public String getMainId() {
+        return mainId;
+    }
+
+    public void setMainId(String mainId) {
+        this.mainId = mainId == null ? null : mainId.trim();
+    }
+
+    public Integer getFuncId() {
+        return funcId;
+    }
+
+    public void setFuncId(Integer funcId) {
+        this.funcId = funcId;
+    }
+
+    public Integer getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Integer enabled) {
+        this.enabled = enabled;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo == null ? null : memo.trim();
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy == null ? null : createBy.trim();
+    }
+
+    public String getCreateDt() {
+        return createDt;
+    }
+
+    public void setCreateDt(String createDt) {
+        this.createDt = createDt == null ? null : createDt.trim();
+    }
+
+    public String getUpdateDt() {
+        return updateDt;
+    }
+
+    public void setUpdateDt(String updateDt) {
+        this.updateDt = updateDt == null ? null : updateDt.trim();
+    }
+}
